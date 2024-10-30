cb3011e79d71cf79b2f4c735751a918076eb7196
Scipher <116547433+Harb1n@users.noreply.github.com>
2024-10-30 15:32:23 +0700

    Add files via upload

---
+++ 29-10 LTHDT/29-10/29-10/Program.cs
@@ -0,0 +1,60 @@
+﻿class A
+{
+    protected int m_a;
+    public A()
+    {
+        m_a = 0;
+    }
+    public A(int a)
+    {
+        m_a = a;
+    }
+    public int getA()
+    {
+        return m_a;
+    }
+
+    public void setA(int a)
+    {
+        m_a = a;
+    }
+}
+class B : A
+{
+
+    private int m_b;
+    public B():base()
+    {
+        m_b = 0;
+    }
+    public B(int a,int b): base(a) // Gọi phương thức tạo lập của lớp a để gán a.
+    {
+        m_b = b;
+
+    }
+    public void setAB(int a,int b)
+    {
+        m_b = b;
+        m_a = a;
+    }
+    public int getB(int b)
+    {
+        return m_b;
+
+    }
+    public void setB(int b)
+    {
+        m_b = b;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        B x = new B();
+        x.setAB(2, 3);
+        Console.WriteLine("A={0}: \tB={1}" ,x.getA(),x.getB());
+
+    }
+
+}
---
+++ 29-10 LTHDT/Su trung ten 29-10/Su trung ten 29-10/Program.cs
@@ -0,0 +1,21 @@
+﻿class A
+{
+    public int m;
+}
+class B : A
+{
+    public int m;
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        B x = new B();
+        x.m = 2;
+        A y = x;
+        y.m = 3;
+
+        Console.WriteLine("a = {0} \tB = {1}", y.m, x.m);
+
+    }
+}
